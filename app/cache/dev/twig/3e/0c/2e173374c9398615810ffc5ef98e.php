274ad0b624255f6b549d8f806d23ed4aacb6216b
RICHARD Eddy <richard.eddy@live.fr>
2012-02-19 17:03:31 +0100

    Update bundle helloBundle

--- app/cache/dev/twig/3e/0c/2e173374c9398615810ffc5ef98e.php
+++ app/cache/dev/twig/3e/0c/2e173374c9398615810ffc5ef98e.php
@@ -8,7 +8,7 @@ class __TwigTemplate_3e0c2e173374c9398615810ffc5ef98e extends Twig_Template
         // line 1
         echo "<html>
 <body>
-   Ceci est un tes avec le moteur de template TWIG!
+   Ceci est un tes avec le moteur de template <b>TWIG</b>!
 </body>
 </html>
 ";
