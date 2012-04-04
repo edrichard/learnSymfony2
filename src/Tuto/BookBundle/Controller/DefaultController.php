da5235737d22e35d1cdf4be24143c816e0ff735f
RICHARD Eddy <richard.eddy@live.fr>
2012-04-04 02:36:48 +0200

    Translating apliccation

--- src/Tuto/BookBundle/Controller/DefaultController.php
+++ src/Tuto/BookBundle/Controller/DefaultController.php
@@ -3,13 +3,35 @@
 namespace Tuto\BookBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 
 class DefaultController extends Controller
 {
     
-    public function indexAction($name)
+    /*public function indexAction($name)
     {
         return $this->render('BookBundle:Default:index.html.twig', array('name' => $name));
+    }*/
+    
+    public function indexAction()
+    {
+        return $this->render('BookBundle:Default:index.html.twig');
+    }
+    
+    public function chooseLanguageAction($language = null)
+    {
+        if($language != null)
+        {
+            // On enregistre la langue en session
+            $this->container->get('session')->setLocale($language);
+        }
+
+        // on tente de rediriger vers la page d'origine
+        $url = $this->container->get('request')->headers->get('referer');  var_dump($url); 
+        if(empty($url)) {
+            $url = $this->container->get('router')->generate('BookBundleHomepage');
+        } //var_dump($url);        exit();
+        return new RedirectResponse($url);
     }
 }
