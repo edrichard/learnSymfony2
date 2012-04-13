9dfce5099fb44149f387fd90af920d800622fec0
RICHARD Eddy <richard.eddy@live.fr>
2012-04-13 16:46:18 +0200

    Translate the menu

--- src/Tuto/BookBundle/Menu/Builder.php
+++ src/Tuto/BookBundle/Menu/Builder.php
@@ -4,17 +4,18 @@ namespace Tuto\BookBundle\Menu;
 
 use Knp\Menu\FactoryInterface;
 use Symfony\Component\DependencyInjection\ContainerAware;
+use Symfony\Bundle\FrameworkBundle\Translation\Translator;
 
 class Builder extends ContainerAware
 {
-    public function mainMenu(FactoryInterface $factory, array $options)
+    public function mainMenu(FactoryInterface $factory, array $options, Translator $translator)
     {
         $menu = $factory->createItem('root');
         $menu->setChildrenAttribute('class', 'nav');
         $menu->setCurrentUri($this->container->get('request')->getRequestUri());
 
-        
-        $menu->addChild('Home', array('route' => 'BookBundleHomepage'));
+        $menu->addChild('Home', array('route' => 'BookBundleHomepage'))
+             ->setLabel($translator->trans('menu.home'));
         $menu->addChild('Books', array('route' => 'book'));
         $menu->addChild('Authors', array('route' => 'author'));
         
