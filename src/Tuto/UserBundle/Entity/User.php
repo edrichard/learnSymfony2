11b9d8bb4295debe8e576422a8b2f7f8c831aa3a
RICHARD Eddy <richard.eddy@live.fr>
2012-04-16 20:34:38 +0200

    generate entities and update the database.

---
+++ src/Tuto/UserBundle/Entity/User.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Tuto\UserBundle\Entity;
+
+use FOS\UserBundle\Entity\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\id
+     * @ORM\Column(type="integer")
+     * @ORM\generatedValue(strategy="AUTO")
+     * 
+     * @var integer
+     */
+    protected $id;
+    
+    function __construct() {
+        parent::__construct();
+    }
+
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+}
